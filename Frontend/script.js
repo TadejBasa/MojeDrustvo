316ac2fa1f09de43493c8cadb8c18efbbd2dc4a9
ŽigaRaus <ziga.raus@student.um.si>
2026-05-20 17:21:44 +0200

    Dodan slider z sponzorji, trianglify ozadje, posodobljen grid

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,115 +1,15 @@
-const dogodki = [
+window.onload = function() {
+  const pattern = trianglify({
+    width: window.innerWidth,
+    height: document.body.scrollHeight,
+    cellSize: 80,
+    xColors: ['#259ad5', '#7928ca', '#ff0080'],
+  });
 
-  {
-    naslov: "Pohod na Pohorje",
-    datum: "15. 6. 2026",
-    lokacija: "Maribor",
-    opis: "Skupinski pohod v naravi za vse generacije.",
-    slika: ""
-  },
-
-  {
-    naslov: "Delavnica prve pomoči",
-    datum: "20. 6. 2026",
-    lokacija: "Celje",
-    opis: "Učenje osnov prve pomoči in reševanja življenj.",
-    slika: ""
-  },
-
-  {
-    naslov: "Čistilna akcija mesta",
-    datum: "25. 6. 2026",
-    lokacija: "Ljubljana",
-    opis: "Skupaj očistimo okolico in naravo.",
-    slika: ""
-  },
-
-  {
-    naslov: "Kolesarski izlet ob Dravi",
-    datum: "28. 6. 2026",
-    lokacija: "Ptuj",
-    opis: "Sproščen kolesarski izlet ob reki Dravi.",
-    slika: ""
-  },
-
-  {
-    naslov: "Fotografska delavnica",
-    datum: "2. 7. 2026",
-    lokacija: "Maribor",
-    opis: "Osnove fotografije in kompozicije.",
-    slika: ""
-  },
-
-  {
-    naslov: "Planinski tabor",
-    datum: "5. 7. 2026",
-    lokacija: "Kranjska Gora",
-    opis: "Večdnevno druženje v planinah.",
-    slika: ""
-  },
-
-  {
-    naslov: "Delavnica zdrave prehrane",
-    datum: "10. 7. 2026",
-    lokacija: "Novo mesto",
-    opis: "Kako pripraviti zdrave in enostavne obroke.",
-    slika: ""
-  },
-
-  {
-    naslov: "Večer družabnih iger",
-    datum: "12. 7. 2026",
-    lokacija: "Maribor",
-    opis: "Sproščen večer druženja in iger.",
-    slika: ""
-  },
-
-  {
-    naslov: "Prostovoljska akcija za otroke",
-    datum: "18. 7. 2026",
-    lokacija: "Koper",
-    opis: "Pomoč in aktivnosti za otroke iz lokalne skupnosti.",
-    slika: ""
-  },
-
-  {
-    naslov: "Tečaj osnov spletnega programiranja",
-    datum: "22. 7. 2026",
-    lokacija: "Ljubljana",
-    opis: "Uvod v HTML, CSS in JavaScript.",
-    slika: ""
-  }
-
-]
-
-const container =
-  document.querySelector("#dogodki-container")
-
-dogodki.forEach((dogodek) => {
-
-  container.innerHTML += `
-  
-    <div class="col-md-6 col-lg-4">
-
-      <div class="dogodek-kartica">
-
-        <h3>
-          ${dogodek.naslov}
-        </h3>
-
-        <p class="datum">
-            ${dogodek.datum} </p>
-
-          <p class="lokacija">
-          ${dogodek.lokacija} </p>
-
-          <p>
-            ${dogodek.opis} </p>
-
-        <button class="btn btn-success">Več</button>
-
-      </div>
-
-    </div>
-  `
-})
+  document.body.style.backgroundImage = `
+    linear-gradient(rgba(255,255,255,0.3), rgba(255,255,255,0.4)),
+    url('data:image/svg+xml;utf8,${encodeURIComponent(pattern.toSVG().outerHTML)}')
+  `;
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundAttachment = 'fixed';
+};
